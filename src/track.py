3b149fbf921059075d25ed2c6e425a144fa887f5
CruzCarlosC <ccc538@alumnos.unican.es>
2024-04-06 17:15:48 +0200

    correccion track

--- src/track.py
+++ src/track.py
@@ -8,25 +8,30 @@ class Track():
         Definir constantes y variables
         """
         self.phi = np.pi/2 #np.random.uniform(0,np.pi)
-        self.eta = 2 #pseudorapidity
-        self.k=0.29979 #GeV/(cTm
-        self.q=-1 #negative or positive factor of e=1.6e-19
-        self.beta=0.8 #units of c
+        self.eta = 1.5 #pseudorapidity
+        self.k=0.29979 #GeV/(cTm)
+        self.q=-1 #negative or positive factor of e
+        self.pt=50 #GeV/c
+        self.pz=20 #GeV/c
+        self.m=0.106 #GeV/c**2
+        self.beta= np.sqrt(self.pt**2 + self.pz**2)/np.sqrt(self.pt**2 + self.pz**2 + self.m**2)
         self.gama=1/(np.sqrt(1-self.beta**2))
-        self.pt=10 #GeV/c
-        self.B=4 #T
-        self.m=0.5e-3 #GeV/c**2
+        self.B=3.88 #T
         self.r=self.pt/(self.k*self.q*self.B)
         self.w=self.k*self.q*self.B/(self.gama*self.m)
+        self.x0=0
+        self.y0=0
+        self.z0=0
 
     def motion(self,t):
         """
         Puntos de la trayectoria de la partícula cargada en un campo magnético
         """
-        x=self.r * (np.cos(self.w*t - self.phi) - np.cos(self.phi))
-        y=self.r * (np.sin(self.w*t - self.phi) + np.sin(self.phi))
-        z=np.sinh(self.eta)*t*self.pt/(self.m * self.gama)
-        return (np.array([x,y,z]))
+        x=self.x0 + self.r * (np.sin(self.w*t - self.phi) + np.sin(self.phi))
+        y=self.y0 + self.r * (np.cos(self.w*t - self.phi) - np.cos(self.phi))
+        z=self.z0 + self.pt*np.sinh(self.eta)*t/(self.m * self.gama)
+        #return (np.array([x,y,z]))
+        return (x,y,z)
 
     def update_motion(self):
         #Empty ndarray
@@ -52,6 +57,22 @@ class Track():
         plt.ylabel('y')
         plt.title('Motion of a charged particle in a magnetic field')
         plt.show()
+    
+    def plot3d(self):
+        t=np.linspace(-10,10,100)
+        x,y,z=self.motion(t)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(x,y,z, label='parametric curve')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.legend()
+
+
  
 if __name__ == "__main__":
     print("Track class")
+    track=Track()
+    track.plot3d()
+    plt.show()
