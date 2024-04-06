7231aeb39f7399f2b5ce07dadc466a66c5d77d27
CruzCarlosC <ccc538@alumnos.unican.es>
2024-04-06 22:34:07 +0200

    actualizacion track

--- src/track.py
+++ src/track.py
@@ -23,44 +23,27 @@ class Track():
         self.y0=0
         self.z0=0
 
-    def motion(self,t):
+    def eq_motion(self,t):
         """
         Puntos de la trayectoria de la partícula cargada en un campo magnético
         """
         x=self.x0 + self.r * (np.sin(self.w*t - self.phi) + np.sin(self.phi))
         y=self.y0 + self.r * (np.cos(self.w*t - self.phi) - np.cos(self.phi))
         z=self.z0 + self.pt*np.sinh(self.eta)*t/(self.m * self.gama)
-        #return (np.array([x,y,z]))
         return (x,y,z)
 
-    def update_motion(self):
-        #Empty ndarray
-        x=np.array([])
-        y=np.array([])
-        #append to the empty ndarray
-        i=0
-        while i<2e-4:
-            x=np.append(x,self.motion(i)[0])
-            y=np.append(y,self.motion(i)[1])
-            i=i+2e-6
-        return (x,y)
-        
-    def update_and_store(self,x,y,dt):
-        x=np.append(x,self.motion(dt)[0])
-        y=np.append(y,self.motion(dt)[1])
-        return (x,y)
-
-    def plot_motion(self):
-        x,y=self.update_motion()
+    def transverse_plot(self):
+        t=np.linspace(-10,10,100)
+        x,y,z=self.eq_motion(t)
         plt.plot(x,y)
         plt.xlabel('x')
         plt.ylabel('y')
-        plt.title('Motion of a charged particle in a magnetic field')
+        plt.title('Transverse plane')
         plt.show()
     
     def plot3d(self):
         t=np.linspace(-10,10,100)
-        x,y,z=self.motion(t)
+        x,y,z=self.eq_motion(t)
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         ax.plot(x,y,z, label='parametric curve')
@@ -73,6 +56,3 @@ class Track():
  
 if __name__ == "__main__":
     print("Track class")
-    track=Track()
-    track.plot3d()
-    plt.show()
