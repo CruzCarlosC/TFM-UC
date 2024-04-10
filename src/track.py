b19b512e5d2bf0aa207478caaec89ab75ccaba6f
CruzCarlosC <ccc538@alumnos.unican.es>
2024-04-10 10:47:49 +0200

    poca function

--- src/track.py
+++ src/track.py
@@ -15,15 +15,15 @@ class Track():
         self.pz=pz #GeV/c
         self.m=0.106 #GeV/c**2
         self.beta= np.sqrt(self.pt**2 + self.pz**2)/np.sqrt(self.pt**2 + self.pz**2 + self.m**2)
-        self.gama=1/(np.sqrt(1-self.beta**2))
+        self.gamma=1/(np.sqrt(1-self.beta**2))
         self.B=3.88 #T
         self.r=self.pt/(self.k*self.q*self.B)
-        self.w=self.k*self.q*self.B/(self.gama*self.m)
+        self.w=self.k*self.q*self.B/(self.gamma*self.m)
         self.x0=x0
         self.y0=y0
         self.z0=z0
 
-    def poca(self):
+    def poca_points(self):
         """
         Punto de aproximación más cercano
         """
@@ -37,14 +37,27 @@ class Track():
         x_poca=self.x0+(self.r)*(np.sin(self.w*t_poca-self.phi)+np.sin(self.phi))
         y_poca=self.y0+(self.r)*(np.cos(self.w*t_poca-self.phi)-np.cos(self.phi))
         z_poca=self.z0+(self.pt/(self.gamma*self.m))*np.sinh(self.eta)*t_poca
+
+        px=self.pt*np.cos(self.w*t_poca-self.phi)
+        py=-self.pt*np.sin(self.w*t_poca-self.phi)
+        phi_poca= np.arctan2(py, px)
+
+        return (x_poca,y_poca,z_poca,phi_poca,t_poca)
     
     def eq_motion(self,t):
         """
         Puntos de la trayectoria de la partícula cargada en un campo magnético
         """
-        x=self.x0 + self.r * (np.sin(self.w*t - self.phi) + np.sin(self.phi))
-        y=self.y0 + self.r * (np.cos(self.w*t - self.phi) - np.cos(self.phi))
-        z=self.z0 + self.pt*np.sinh(self.eta)*t/(self.m * self.gama)
+        xp,yp,zp,phi_poca,t_poca=self.poca_points()
+
+        x=xp+(self.r)*(np.sin(self.w*(t-t_poca)-phi_poca)+np.sin(phi_poca))
+        y=yp+(self.r)*(np.cos(self.w*(t-t_poca)-phi_poca)-np.cos(phi_poca))
+        z=zp+(self.pt/(self.gamma*self.m))*np.sinh(self.eta)*(t-t_poca)
+
+        #Ecuacion sin modificar
+        #x=self.x0 + self.r * (np.sin(self.w*t - self.phi) + np.sin(self.phi))
+        #y=self.y0 + self.r * (np.cos(self.w*t - self.phi) - np.cos(self.phi))
+        #z=self.z0 + self.pt*np.sinh(self.eta)*t/(self.m * self.gama)
         return (x,y,z)
 
     def transverse_plot(self):
